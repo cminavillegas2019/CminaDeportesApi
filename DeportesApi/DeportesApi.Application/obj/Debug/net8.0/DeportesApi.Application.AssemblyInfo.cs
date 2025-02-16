aaef35fb52710acbdab80e9a97212743af3887dc
cminavillegas2019 <cmina_villegas@hotmail.com>
2025-02-16 14:18:14 -0500

    Base

--- DeportesApi/DeportesApi.Application/obj/Debug/net8.0/DeportesApi.Application.AssemblyInfo.cs
+++ DeportesApi/DeportesApi.Application/obj/Debug/net8.0/DeportesApi.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeportesApi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5eee831ac467de14306490dbeb1fd7059798d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeportesApi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeportesApi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DeportesApi/DeportesApi.Infrastructure/obj/Debug/net8.0/DeportesApi.Infrastructure.AssemblyInfo.cs
+++ DeportesApi/DeportesApi.Infrastructure/obj/Debug/net8.0/DeportesApi.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeportesApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5eee831ac467de14306490dbeb1fd7059798d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeportesApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeportesApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
